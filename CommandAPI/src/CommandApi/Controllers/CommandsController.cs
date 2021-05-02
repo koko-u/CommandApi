293be170fd27910c9e64780798125374b735eb6e
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-05-03 08:50:33 +0900

    create hard coded get endpoint

---
+++ CommandAPI/src/CommandApi/Controllers/CommandsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[]
+            {
+                "this", "is", "hard", "coded"
+            };
+        }
+    }
+}
