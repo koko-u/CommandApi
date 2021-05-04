c60a2c92f5635c6f92ca76a6233792503fca5591
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-05-04 09:14:15 +0900

    Introduce DTO and AutoMapper

---
+++ CommandAPI/src/CommandApi/Profiles/CommandsProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CommandApi.Dtos;
+using CommandApi.Models;
+
+namespace CommandApi.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        /// <inheritdoc />
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
